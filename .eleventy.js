4e99de2f8cd95165e33d8ecbde181bb26becf5a8
redpandaathome <leeyumi415@gmail.com>
2022-07-08 13:47:44 +0100

    Fixing the date formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
-module.exports = function(eleventyConfig) {
-   eleventyConfig.addPassthroughCopy("./src/style.css");
-   eleventyConfig.addPassthroughCopy("./src/assets");
+const { DateTime } = require("luxon");
 
-   return {
-     dir: {
-       input: "src",
-       output: "public"
-     }
-   }
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
